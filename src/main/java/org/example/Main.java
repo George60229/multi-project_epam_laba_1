39dafccd1756882dd2d4a33f05d008b39a79b58d
George60229 <174180183george>
2022-12-21 14:13:20 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Utils res=new Utils();
+        System.out.println(res.isAllPositiveNumbers("5","6","9"));
+    }
+}
