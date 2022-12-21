02f68bb28720dbf315f028d1f36f5a8fb8d0e519
George60229 <174180183george>
2022-12-21 15:06:41 +0200

    All works without modules

--- core/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Utils res=new Utils();
-        System.out.println(res.isAllPositiveNumbers("5","6","9"));
-    }
-}
--- core/src/main/java/org/example/Utils.java
+++ core/src/main/java/org/example/Utils.java
@@ -3,7 +3,7 @@ package org.example;
 public class Utils {
     static final StringUtils utils = new StringUtils();
 
-    boolean isAllPositiveNumbers(String... str) {
+    static boolean isAllPositiveNumbers(String... str) {
 
 
         for (String s : str) {
